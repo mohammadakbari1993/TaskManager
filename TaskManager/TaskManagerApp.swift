17e79fcca197233a532722b87eb6b79490f3c4b0
Mohammad Akbari <mohammad.a@nizek.com>
2023-07-18 19:44:22 +0330

    Initial Commit

---
+++ TaskManager/TaskManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskManagerApp.swift
+//  TaskManager
+//
+//  Created by Mohammad Akbari on 4/27/1402 AP.
+//
+
+import SwiftUI
+
+@main
+struct TaskManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
